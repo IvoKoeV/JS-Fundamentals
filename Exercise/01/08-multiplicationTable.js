92aa58ac57c1ddb7a90250e7fc2a087f021eadb3
IvoKoeV <126709829+IvoKoeV@users.noreply.github.com>
2023-09-21 16:55:44 +0300

    Add files via upload

---
+++ Exercise/01/08-multiplicationTable.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num){
+    for(let i=0;i<1;i++){
+        
+        for(let j=1;j<=10;j++){
+        console.log(num + " X " + j + ' = ' + num *j)
+        }
+        
+    }
+}
+multiplicationTable(2)

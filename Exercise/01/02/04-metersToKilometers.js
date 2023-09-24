a8b2add378d5b63b7e0ddb88b29fd8b66cee4e8f
IvoKoeV <126709829+IvoKoeV@users.noreply.github.com>
2023-09-25 00:06:06 +0300

    Delete Exercise/01 directory

--- Exercise/01/02/04-metersToKilometers.js
+++
@@ -1,6 +0,0 @@
-function mToK(meters){
-    let kilometers = 1000;
-    let result = meters/kilometers;
-    console.log(result.toFixed(2));
-}
-mToK(798)
--- Exercise/01/02/05-poundToDollars.js
+++
@@ -1,6 +0,0 @@
-function poundToDollar(pound){
-    let dollars = 1.31;
-    let result = pound*dollars;
-    console.log(result.toFixed(3));
-}
-poundToDollar(39)
